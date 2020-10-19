93ff8e805a8ee39c6df0d257d0ef22f189c6d7c6
Doyle Shaw <shawdoyl@isu.edu>
2020-10-19 08:28:19 -0600

    Create an Etch a Sketch

---
+++ Etch-a-Sketch/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.WaveformButton = New System.Windows.Forms.Button()
+        Me.ColorButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox
+        '
+        Me.PictureBox.BackColor = System.Drawing.Color.Silver
+        Me.PictureBox.Location = New System.Drawing.Point(12, 38)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(489, 305)
+        Me.PictureBox.TabIndex = 0
+        Me.PictureBox.TabStop = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(507, 236)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(164, 60)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'WaveformButton
+        '
+        Me.WaveformButton.Location = New System.Drawing.Point(507, 104)
+        Me.WaveformButton.Name = "WaveformButton"
+        Me.WaveformButton.Size = New System.Drawing.Size(164, 60)
+        Me.WaveformButton.TabIndex = 2
+        Me.WaveformButton.Text = "Draw Waveforms"
+        Me.WaveformButton.UseVisualStyleBackColor = True
+        '
+        'ColorButton
+        '
+        Me.ColorButton.Location = New System.Drawing.Point(507, 38)
+        Me.ColorButton.Name = "ColorButton"
+        Me.ColorButton.Size = New System.Drawing.Size(164, 60)
+        Me.ColorButton.TabIndex = 3
+        Me.ColorButton.Text = "Color"
+        Me.ColorButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(507, 170)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(164, 60)
+        Me.ClearButton.TabIndex = 4
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(681, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(681, 358)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ColorButton)
+        Me.Controls.Add(Me.WaveformButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PictureBox)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox As PictureBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents WaveformButton As Button
+    Friend WithEvents ColorButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ Etch-a-Sketch/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+    Private Sub GraphicsForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+
+        Me.Text = "x: " & e.X & " y: " & e.Y & " Mouse Button: " & e.Button.ToString
+
+        'create graphics object
+        Dim graph As Graphics
+        graph = Me.CreateGraphics
+
+        Dim penColor As Color
+        If penColor.IsEmpty = True Then
+            penColor = Color.Black
+        End If
+
+        'constructor for pen object
+        Dim myPen As New Pen(penColor)
+
+        Static oldX As Integer
+        Static oldY As Integer
+
+        If e.Button.ToString = "Left" Then
+            'draw the line
+            graph.DrawLine(myPen, oldX, oldY, e.X, e.Y)
+        ElseIf e.Button.ToString = "Middle" Then
+            ColorDialog1.ShowDialog()
+            penColor = ColorDialog1.Color
+            myPen.Color = penColor
+        End If
+
+        'store position
+        oldX = e.X
+        oldY = e.Y
+
+        'free up resources
+        myPen.Dispose()
+        graph.Dispose()
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub WaveformButton_Click(sender As Object, e As EventArgs) Handles WaveformButton.Click
+
+    End Sub
+
+    Private Sub ColorButton_Click(sender As Object, e As EventArgs) Handles ColorButton.Click
+        Static penColor As Color
+        Dim myPen As New Pen(penColor)
+
+        ColorDialog1.ShowDialog()
+        penColor = ColorDialog1.Color
+        myPen.Color = penColor
+    End Sub
+End Class
